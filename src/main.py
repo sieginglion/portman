c3c8f66dd7275ff9c7d1ccd20d2caf36ef62b3e3
sieginglion <sieginglion@gmail.com>
2023-02-12 11:05:34 +0000

    macd

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ class Derived(TypedDict):
     ER: f8
     omega: f8
     downside: f8
-    signal: Literal[-1, 0, 1]
+    signal: f8 | None
 
 
 @app.get('/ranking')
@@ -21,27 +21,14 @@ def get_ranking(k: int) -> HTMLResponse:
 
 @app.get('/derived')
 async def get_derived(
-    market: Literal['c', 't', 'u'], symbol: str, scale: int, theta: float = 0.0
+    market: Literal['c', 't', 'u'],
+    symbol: str,
+    m_scale: int,
+    s_scale: int,
+    theta: float,
 ) -> Derived:
-    p = await position.Position(market, symbol, scale)
-    return {**p.calc_metrics(theta), 'signal': p.calc_signal()}
-
-
-@app.get('/margin')
-async def get_margin(market: Literal['c', 't', 'u'], symbol: str) -> f8:
-    w = 364 * 8
-    k = calc_k(w)
-    n = 364 * 4 + k - 1
-    P = await (
-        crypto.get_prices(symbol, n)
-        if market == 'c'
-        else stock.get_prices(market, symbol, n)
-    )
-    ema = calc_ema(P, 2 / (w + 1), k)
-    M = 1 - P[-len(ema) :] / ema
-    p50, p99 = np.percentile(M, 50), np.percentile(M, 99)
-    M = 10 / 12 + 2 / 12 * (M - p50) / (p99 - p50)
-    return M[-1]
+    p = await position.Position(market, symbol, m_scale, s_scale)
+    return {**p.calc_metrics(theta), 'signal': p.search_signal() if s_scale else None}
 
 
 if __name__ == '__main__':
--- src/position.py
+++ src/position.py
@@ -3,26 +3,30 @@ import stock
 from shared import *
 
 
-@nb.njit(parallel=True)
-def calc_trends(prices: Array[f8], a: float, k: int) -> Array[f8]:
-    ema = calc_ema(prices, a, k)
-    return np.sign(ema[1:] - ema[:-1])
+def calc_signals(prices: Array[f8], w_s, w_l) -> Array[f8]:
+    s_ema = calc_ema(prices, 2 / (w_s + 1), calc_k(w_s))
+    l_ema = calc_ema(prices, 2 / (w_l + 1), calc_k(w_l))
+    macd = s_ema[-len(l_ema) :] - l_ema
+    slow = calc_ema(macd, 2 / (7 + 1), calc_k(7))
+    macd = macd[-len(slow) :]
+    signals = np.zeros(len(macd))
+    signals[(macd < 0) & (macd > slow)] = 1
+    signals[(macd > 0) & (macd < slow)] = -1
+    return signals
 
 
 @nb.njit
-def simulate(prices: Array[f8], trends: Array[f8]) -> float:
+def simulate(prices: Array[f8], signals: Array[f8]) -> float:
     last_trade, earned = 0, 0
     cash, position = 1000, 0
-    last_price = 0
-    for price, trend in zip(prices, trends):
-        if price != last_price:
-            if trend > 0 and cash > 0:
+    for i, price in enumerate(prices):
+        if i and price != prices[i - 1]:
+            if cash > 0 and signals[i] > 0:
                 last_trade = price
                 cash, position = 0, cash / price
-            elif trend < 0 and cash == 0:
+            elif cash == 0 and signals[i] < 0:
                 earned += position * (price - last_trade)
                 cash, position = position * price, 0
-        last_price = price
     return earned
 
 
@@ -33,13 +37,16 @@ class Metrics(TypedDict):
 
 
 class Position:
-    def __init__(self, market: Literal['c', 't', 'u'], symbol: str, scale: int) -> None:
+    def __init__(
+        self, market: Literal['c', 't', 'u'], symbol: str, m_scale: int, s_scale: int
+    ) -> None:
         self.market = market
         self.symbol = symbol
-        self.scale = scale
+        self.m_scale = m_scale
+        self.s_scale = s_scale
 
     async def ainit(self):
-        n = self.scale + 1
+        n = self.m_scale + 1  # TODO: take s_scale into consideration
         self.prices = await (
             crypto.get_prices(self.symbol, n)
             if self.market == 'c'
@@ -52,7 +59,7 @@ class Position:
     def __await__(self):
         return self.ainit().__await__()
 
-    def calc_metrics(self, theta: float = 0.0) -> Metrics:
+    def calc_metrics(self, theta: float = 0) -> Metrics:
         R = np.log(self.prices[1:] / self.prices[:-1]) - theta
         return {
             'ER': np.mean(R) + theta,
@@ -60,18 +67,25 @@ class Position:
             'downside': (np.sum(R[R < 0] ** 2) / len(R)) ** 0.5,
         }
 
-    def calc_signal(self) -> Literal[-1, 0, 1]:
-        s, W = 182, [7, 14, 28]
-        w_to_trends = {w: calc_trends(self.prices, 2 / (w + 1), calc_k(w)) for w in W}
-        w_to_score = {w: simulate(self.prices[-s:], w_to_trends[w][-s:]) for w in W}
-        w = max(w_to_score.items(), key=lambda x: x[1])[0]
-        return w_to_trends[w][-1]
+    def search_signal(self) -> f8:
+        s, W = self.s_scale + 1, [7, 14, 28]
+        W_to_score = {
+            (w_s, w_l): simulate(
+                self.prices[-s:],
+                calc_signals(self.prices, w_s, w_l)[-s:],
+            )
+            for w_s in W
+            for w_l in W
+            if w_s < w_l
+        }
+        (w_s, w_l), score = max(W_to_score.items(), key=lambda x: x[1])
+        return calc_signals(self.prices, w_s, w_l)[-1]
 
 
 # async def main():
-#     p = await Position('c', 'ETH', 364 * 2)
+#     p = await Position('c', 'ETH', 364 * 2, 182)
 #     print(p.calc_metrics())
-#     print(p.calc_signal())
+#     print(p.search_signal())
 
 
 # asyncio.run(main())
