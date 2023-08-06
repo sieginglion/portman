23c9a81ab8b6ab14e7c6d1a83cedb3b415776b2f
sieginglion <sieginglion@gmail.com>
2023-08-06 15:51:57 +0800

    monorepo

--- bin/restart-backend.sh
+++ bin/restart-backend.sh
@@ -4,4 +4,4 @@ grep -q TWT48U.json /etc/crontab ||
     (echo "0 9 * * *	root	curl -o $(pwd)/TWT48U.json https://www.twse.com.tw/rwd/zh/exRight/TWT48U?response=json" | sudo tee -a /etc/crontab)
 
 pkill -f uvicorn
-poetry run -- uvicorn --host 0.0.0.0 --port 8080 portman.main:app 1>portman.log 2>&1 &
+poetry run -- uvicorn --host 0.0.0.0 --port 8080 --workers 2 backend.main:app 1>backend.log 2>&1 &
